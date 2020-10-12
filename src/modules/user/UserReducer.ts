8067a3dcc6c958eb28cc3d34f06bf7243374966f
Oleg <oleg.h@develops.today>
2020-10-12 18:44:58 +0300

    feat(user): add logic to store user from fetchUser payload

--- src/modules/user/UserReducer.ts
+++ src/modules/user/UserReducer.ts
@@ -1,19 +1,49 @@
-import { FETCH_USERS, FETCH_USERS_SUCCESS } from './UserActions';
+import {
+  FETCH_USER,
+  FETCH_USERS,
+  FETCH_USERS_SUCCESS,
+  FETCH_USER_SUCCESS,
+} from './UserActions';
 
 export const STATE_KEY = 'user';
 
 const initialState = {
-  loading: false,
-  data: [],
+  isLoading: false,
+  data: {},
+  lastUserId: '',
 };
 
 const UserReducer = (state = initialState, action: Record<string, any>) => {
   switch (action.type) {
+    case FETCH_USER:
     case FETCH_USERS: {
-      return { ...state, loading: true };
+      return { ...state, isLoading: true };
+    }
+    case FETCH_USER_SUCCESS: {
+      const user = action.payload.data;
+      const data = {
+        ...state.data,
+        [user.login]: user,
+      };
+
+      return { ...state, isLoading: false, data };
     }
     case FETCH_USERS_SUCCESS: {
-      return { ...state, loading: false, data: action.payload.data };
+      const users = action.payload.data;
+      const indexedUsers = users.reduce(
+        (res: Record<string, any>, val: any) => ({
+          ...res,
+          [val.login]: val,
+        }),
+        {},
+      );
+
+      return {
+        ...state,
+        isLoading: false,
+        data: indexedUsers,
+        lastUserId: [...users].pop().id,
+      };
     }
     default: {
       return state;
@@ -21,8 +51,14 @@ const UserReducer = (state = initialState, action: Record<string, any>) => {
   }
 };
 
-export const getLoadingUsers = (state: any) => state[STATE_KEY].loading;
+export const getIsLoadingUsers = (state: any) => state[STATE_KEY].isLoading;
 
-export const getUsers = (state: any) => state[STATE_KEY].data;
+export const getUsers = (state: any): any =>
+  Object.values(state[STATE_KEY].data);
+
+export const getUserById = (state: any, id: string) =>
+  state[STATE_KEY].data[id];
+
+export const getLastUserId = (state: any) => state[STATE_KEY].lastUserId;
 
 export default UserReducer;
