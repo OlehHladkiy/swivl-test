6ca95eb580e4267679e4a48501f17bd984c29568
Oleg <oleg.h@develops.today>
2020-10-12 18:44:05 +0300

    feat(user): implement action creators to fetch user

--- src/modules/user/UserActions.ts
+++ src/modules/user/UserActions.ts
@@ -1,15 +1,25 @@
 import axios from 'axios';
 
+import apiConfig from 'config/apiConfig';
+
 export const namespace = 'USERS';
 export const FETCH_USERS = `${namespace}/FETCH_USERS`;
 export const FETCH_USERS_SUCCESS = `${namespace}/FETCH_USERS_SUCCESS`;
+export const FETCH_USER = `${namespace}/FETCH_USER`;
+export const FETCH_USER_SUCCESS = `${namespace}/FETCH_USER_SUCCESS`;
 
-export const fetchUsers = async (
-  url = 'https://api.github.com/users',
-) => async (dispatch: any) => {
+export const fetchUsers = () => async (dispatch: any) => {
   dispatch({ type: FETCH_USERS });
 
-  const payload = await axios.get(url);
+  const payload = await axios.get(`${apiConfig.apiUrl}/users`);
 
   dispatch({ type: FETCH_USERS_SUCCESS, payload });
+};
+
+export const fetchUser = (login: string) => async (dispatch: any) => {
+  dispatch({ type: FETCH_USER });
+
+  const payload = await axios.get(`${apiConfig.apiUrl}/users/${login}`);
+
+  dispatch({ type: FETCH_USER_SUCCESS, payload });
 };
