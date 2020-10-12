b1944a4a042db628575343bb29710a830becfd6e
Oleg <oleg.h@develops.today>
2020-10-12 18:42:21 +0300

    chore(user): define type for user

---
+++ src/modules/user/models/user-model.ts
@@ -0,0 +1,15 @@
+export type UserModel = {
+  login: string;
+  html_url: string;
+  avatar_url: string;
+  id: number;
+  node_id: string;
+  followers: number;
+  following: number;
+  created_at: Date;
+  company: string;
+  email: string;
+  location: string;
+  blog: string;
+  bio: string;
+};
